31726c22014026234dc7a75bcda1c63b84d05fd1
Unknown <ferdyh97@hotmail.nl>
2019-09-06 11:32:52 +0200

    Final landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Made by Ferdy</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -61,7 +61,7 @@
             .subtitle {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 21px;
+                font-size: 16px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
